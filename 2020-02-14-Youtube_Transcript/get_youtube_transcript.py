367e7a2c17013c03c4f66f4fe18de7852565d4c0
I Kit Cheng <matthewkit@gmail.com>
2022-04-15 17:06:23 +0800

    [UPDATE] New xpaths for opening and copying transcript [UPDATE] wrap everything inside main function [UPDATE] Latest stable chromedriver.exe ChromeDriver 100.0.4896.60

--- 2020-02-14-Youtube_Transcript/get_youtube_transcript.py
+++ 2020-02-14-Youtube_Transcript/get_youtube_transcript.py
@@ -11,34 +11,24 @@ Description: get transcript (auto-generated) from any youtube video.
 
 import pandas as pd
 from time import sleep
+import os
 from selenium import webdriver # for interacting with website
 
 def open_url_in_chrome(url, mode='headed'):
     #print(f'Opening {url}')
     if mode == 'headed':
-        options = webdriver.ChromeOptions()
-        #options.add_argument("start-maximized")
-            
+        driver = webdriver.Chrome()
     elif mode == 'headless':   
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
-        
-    options.add_argument("--auto-open-devtools-for-tabs")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    driver = webdriver.Chrome('./chromedriver.exe', options=options)
+        driver = webdriver.Chrome('./chromedriver.exe', options=options)
     
     driver.get(url)
     return driver
 
 def accept_T_and_C(driver):
-    """
-    Accept terms and conditions.
-    
-    ### Old Code ###
     # Click 'No thanks'
-    driver.find_element_by_xpath("//paper-button[@aria-label='No thanks']").click() # old
-    driver.find_element_by_xpath("//*[@id='dismiss-button']").click() #new
+    driver.find_element_by_xpath("//paper-button[@aria-label='No thanks']").click()
     
     # Click 'I agree' https://stackoverflow.com/questions/64846902/how-to-get-rid-of-the-google-cookie-pop-up-with-my-selenium-automation
     driver.switch_to.frame(driver.find_element_by_xpath("//iframe[contains(@src, 'consent.google.com')]"))
@@ -46,31 +36,21 @@ def accept_T_and_C(driver):
     driver.find_element_by_xpath('//*[@id="introAgreeButton"]/span/span').click()
     sleep(3)
     driver.refresh()
-    #################
-    
-    """
-    # Click I agree
-    driver.find_element_by_xpath("//*[@id='yDmH0d']/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/div[2]").click()
-    sleep(2)
-    try:
-        # click 'no thanks' if it pops up
-        driver.find_element_by_xpath("//*[@id='dismiss-button']").click()
-    except:
-        sleep(2)
     
 def get_transcript(driver, mode):
-    
-    global count 
     
     driver.implicitly_wait(10)
     
     if mode=='headed':
-        print('Accepting Terms and Conditions')
-        accept_T_and_C(driver)
+        try:
+            print('Accepting Terms and Conditions')
+            accept_T_and_C(driver)
+        except:
+            print("No T&Cs to accept.")
         
-        # Click 'More actions' (full xpath)
-        #driver.find_elements_by_xpath("//button[@aria-label='More actions']")[1].click()
-        driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[8]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/yt-icon-button/button").click()
+        print("Opening transcript")
+        # Click 'More actions'
+        driver.find_element_by_xpath("//button[@aria-label='More actions']").click() 
         
         # Click 'Open transcript'
         driver.find_element_by_xpath("//*[@id='items']/ytd-menu-service-item-renderer/tp-yt-paper-item").click()
@@ -79,44 +59,29 @@ def get_transcript(driver, mode):
     elif mode=='headless':
         # Click 'More actions'
         try:
-            driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[8]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/yt-icon-button/button").click()
-
+            driver.find_elements_by_xpath("//button[@aria-label='More actions']")[1].click()
         except:
             sleep(3)
-            count += 1
-            if count < 5:
-                driver.refresh()
-                get_transcript(driver, mode)
-            else:
-                print("Error loading page.")
-                return None
+            driver.refresh()
+            get_transcript(driver, mode)
         
         # Click 'open transcript'
         try:
-            #driver.find_element_by_xpath("//*[@id='items']/ytd-menu-service-item-renderer/paper-item").click()
             driver.find_element_by_xpath("//*[@id='items']/ytd-menu-service-item-renderer/tp-yt-paper-item").click()
-
         except:
             sleep(3)
-            count += 1
-            if count < 5:
-                driver.refresh()
-                get_transcript(driver, mode)
-            else: 
-                print("Error loading page.")
-                return None
-            
+            driver.refresh()
+            get_transcript(driver, mode)
     
     # Get all transcript text
     print("Copying transcript ")
-    transcript_element = driver.find_element_by_xpath("//*[@id='body']/ytd-transcript-body-renderer")
+    transcript_element = driver.find_element_by_xpath("//*[@id='body']/ytd-transcript-segment-list-renderer")
     transcript = transcript_element.text
 
     return transcript
 
 def transcript2df(transcript):
-    if transcript == None:
-        return "None in transcript."
+
     transcript = transcript.split('\n')
     transcript_timestamps = transcript[::2]
     
@@ -126,23 +91,30 @@ def transcript2df(transcript):
     
     return df
 
-if __name__ == '__main__':
-    url = "https://www.youtube.com/watch?v=5tvmMX8r_OM"
-    
-    mode = 'headed'
-    
+def main(url, mode='headless'):
     driver = open_url_in_chrome(url, mode)
     
-    count = 0
     transcript = get_transcript(driver, mode)
     
     driver.close()
     	
     df = transcript2df(transcript)
     
+    # Existing list of unique ingredients
+    if not os.path.exists("./output"):
+        os.makedirs("./output")
+
     print('Saving transcript ')
-    df.to_csv('out_transcript_timestamped.csv', index=False) 
-    with open("out_transcript_text_only.txt", "w") as text_file:
+    path_to_transcript = "./output/"
+    df.to_csv(f"{path_to_transcript}my_transcript_timestamped.csv", index=False) 
+    with open(f"{path_to_transcript}my_transcript_text_only.txt", "w") as text_file:
         print(" ".join(" ".join(df.text.values).split()), file=text_file)
-    print('Done')
+    print(f"Transcript saved to: {path_to_transcript}")
+
+if __name__ == '__main__':
+    url = "https://www.youtube.com/watch?v=5tvmMX8r_OM"
+    mode = 'headed'
+    main(url, mode)
+    
+
     
